bcd24e94a9a562aa77d0cce33177b0172a1678ae
Said Kadrioski <said@kadrioski.de>
2023-05-13 10:38:50 +0200

    I dunno what I did here and what not.

---
+++ config.mk
@@ -0,0 +1,32 @@
+# tabbed version
+VERSION = 0.6
+
+# Customize below to fit your system
+
+# paths
+PREFIX = /usr/local
+BINPREFIX = ${HOME}/.local/bin
+MANPREFIX = ${HOME}/.local/man
+
+X11INC = ${PREFIX}/include
+X11LIB = ${PREFIX}/lib
+
+# freetype
+FREETYPELIBS = -lfontconfig -lXft
+FREETYPEINC = ${X11INC}/freetype2
+
+# includes and libs
+INCS = -I. -I/usr/include -I$(X11INC) -I${FREETYPEINC}
+LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 ${FREETYPELIBS} -lXrender
+
+# flags
+CPPFLAGS = -DVERSION=\"${VERSION}\" -D_DEFAULT_SOURCE
+CFLAGS = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
+LDFLAGS = -s ${LIBS}
+
+# Solaris
+#CFLAGS = -fast ${INCS} -DVERSION=\"${VERSION}\"
+#LDFLAGS = ${LIBS}
+
+# compiler and linker
+CC = cc
